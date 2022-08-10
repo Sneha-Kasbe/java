d3169e71b5f67f99651a59558ef90b35d7b1b34b
sneha-kasbe <snehakasbe44@gmail.com>
2022-08-10 23:43:35 +0530

    For Temp
    For temp

---
+++ Array_Program/src/com/array/StringToChar copy.java
@@ -0,0 +1,22 @@
+package com.array;
+
+public class StringToChar {
+
+	public static void main(String[] args) {
+
+		String s = "sneha";
+
+		char[] ch = new char[s.length()];
+
+		for (int i = 0; i < s.length(); i++) {
+
+			ch[i] = s.charAt(i);
+		}
+
+		for (char c : ch) {
+
+			System.out.println(c);
+		}
+	}
+
+}
